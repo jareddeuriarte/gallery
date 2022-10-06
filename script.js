291289e0512fde6e2edacba3d325a1bb4756c9c9
Jared de Uriarte <jared.deuri@gmail.com>
2022-10-06 01:23:10 -0400

    trashed node. owl cdn. working carousel now

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log('hello world')
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:5,
+    dots: true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:2
+        }
+    }
+})
